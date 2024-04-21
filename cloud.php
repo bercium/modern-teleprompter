29322e72498c7d1cd156c0d0b6c28059068aae9f
bercium <bercium@gmail.com>
2024-04-21 21:19:10 +0200

    Remote control feature

--- cloud.php
+++ cloud.php
@@ -3,23 +3,6 @@ header('Access-Control-Allow-Origin: *'); // Allows requests from any origin
 header('Content-Type: application/json'); // Specifies the content type as JSON
 
 
-// Create PDO instance
-/*
-try {
-	$pdo = new PDO('mysql:host=localhost;dbname=udemy_analytics', 'schedule', 'S4cJFuzhWy9WaPsp', [
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-	]);
-    /*
-	$pdo = new PDO('mysql:host=127.0.0.1;dbname=u503204422_udemy', 'u503204422_udemy', 'gXQ3Ak]tG^8x', [
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-	]); //* /
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());  
-}*/
-
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   if (!empty($_POST['teleprompter_remote_code'])){
     $code = strtoupper($_POST['teleprompter_remote_code']);
